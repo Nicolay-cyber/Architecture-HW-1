5c74d656c2cf1198c76eb128b5460f047ec8ddfa
Nikolay <dukarev1@gmail.com>
2022-08-29 20:34:07 +0300

    Homework 5

--- web-server/src/main/java/ru/geekbrains/handler/MethodHandlerFactory.java
+++ web-server/src/main/java/ru/geekbrains/handler/MethodHandlerFactory.java
@@ -6,7 +6,9 @@ import ru.geekbrains.config.ServerConfig;
 import ru.geekbrains.service.FileService;
 import ru.geekbrains.service.SocketService;
 
-import java.util.Set;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public final class MethodHandlerFactory {
 
@@ -16,8 +18,15 @@ public final class MethodHandlerFactory {
         return new GetMethodHandler(postHandler, socketService, responseSerializer, config, fileService);
     }
 
-//    public static MethodHandler createAnnotated() {
-//        Reflections reflections = new Reflections("ru.geekbrains.handler");
-//        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Handler.class);
-//    }
+    public static MethodHandler createAnnotated() {
+        Reflections reflections = new Reflections("ru.geekbrains.handler");
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Handler.class);
+
+        Set<Class<?>> sortedMethods = classes.stream()
+                .sorted(Comparator.comparing(m -> m.getAnnotation(Handler.class).order()))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        sortedMethods.forEach(System.out::println);
+        return null;
+    }
 }
